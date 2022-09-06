dfbe3e31309ffa69b52edcd1110e9a4022954012
Gabriel <105824688+Custodio97@users.noreply.github.com>
2022-09-05 22:06:10 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,125 @@
+const wrapper = document.querySelector(".sliderWrapper")
+const menuItems = document.querySelectorAll(".menuItem")
+console.log(wrapper)
+
+
+const products = [
+    {
+        id: 1,
+        title: "Air Force",
+        price: 119,
+        colors: [
+            {
+                code: "black",
+                img:'./img/air.png'
+            },
+            {
+                code: "darkblue",
+                img:"./img/air2.png",
+            },
+        ],
+    },
+    {
+        id: 2,
+        title: "Air Jordan",
+        price: 149,
+        colors: [
+            {
+                code: 'lightgray',
+                img:'./img/jordan.png'
+            },
+            {
+                code: "green",
+                img:"./img/jordan2.png"
+            }
+        ]
+    },
+    {
+        id: 3,
+        title: "Blazer",
+        price: 109,
+        colors: [
+            {
+                code: "lightgray",
+                img:"./img/blazer.png"
+            },
+            {
+                code: "green",
+                img:"./img/blazer2.png"
+            },
+        ],
+    },
+    {
+        id: 4,
+        title: "Crater",
+        price: 129,
+        colors: [
+            {
+                code: "black",
+                img:"./img/crater.png"
+            },
+            {
+                code: "lightgray",
+                img:"./img/crater2.png"
+            },
+        ],
+    },
+    {
+        id: 5,
+        title: "Hippie",
+        price: 99,
+        colors: [
+            {
+                code: "gray",
+                img:"./img/hippie.png"
+            },
+            {
+                code:"black",
+                img:"./img/hippie2.png"
+            }
+        ]
+    }
+]
+
+let chooseProduct = products[0]
+
+const currentProductImg = document.querySelector(".productImg")
+const currentProductTitle = document.querySelector(".productTitle")
+const currentProductPrice = document.querySelector(".productPrice")
+const currentProducColors = document.querySelectorAll(".color")
+const currentProductSize=document.querySelectorAll(".size")
+
+menuItems.forEach((item, index) => {
+    item.addEventListener("click", () => {
+        wrapper.style.transform = `translateX(${-100 * index}vw)`
+
+        chooseProduct = products[index]
+        
+
+        currentProductTitle.textContent = chooseProduct.title
+        currentProductPrice.textContent = "$" + chooseProduct.price
+        currentProductImg.src = chooseProduct.colors[0].img
+        
+        currentProducColors.forEach((color, index) => {
+            {
+                color.style.backgroundColor=chooseProduct.color[index].code
+            }
+        })
+    })
+})
+
+currentProducColors.forEach((color, index) => {
+    color.addEventListener("click", () => {
+        currentProductImg.src=chooseProduct.colors[index].img
+    })
+})
+const productButton=document.querySelector(".productButton")
+const paymnet = document.querySelector(".payment")
+const close = document.querySelector(".close")
+productButton.addEventListener("click", () => {
+    paymnet.style.display="flex"
+})
+
+close.addEventListener("click", () => {
+    paymnet.style.display="none"
+})
